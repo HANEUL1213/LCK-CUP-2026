71793bb78e09d0e2184e80d5b4ca90b063c3249b
HANEUL1213 <castlehye3@gmail.com>
2026-01-06 16:37:29 +0900

    Create tailwind.config.js

---
+++ app/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./page.tsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
